d320f1cee927d182ffb85d7b0054d611a17f3cf5
lucas <mr.pine97@gmail.com>
2022-03-11 23:04:58 -0300

    req05 - impl: cria a classe Ranger

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,19 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Ranger extends Archetype {
+  static numberOfInstances = 0;
+  energy: EnergyType = 'stamina';
+  constructor(name: string) {
+    super(name);
+    Ranger.numberOfInstances += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.numberOfInstances;
+  }
+
+  get energyType(): EnergyType {
+    return this.energy;
+  }
+}
