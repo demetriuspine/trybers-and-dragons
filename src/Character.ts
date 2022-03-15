92fe8ac4fd78b389189489229c90b296567a8e81
lucas <mr.pine97@gmail.com>
2022-03-14 21:26:01 -0300

    req06 - impl: adiciona atributos na classe Character

---
+++ src/Character.ts
@@ -0,0 +1,15 @@
+import Archetype from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race from './Races';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+}
