af901013a7d2e09fb3700dd0332e46a35a371223
lucas <mr.pine97@gmail.com>
2022-03-15 16:13:39 -0300

    req06 - impl: adiciona os metodos na classe Character

--- src/Character.ts
+++ src/Character.ts
@@ -58,9 +58,42 @@ export default class Character implements Fighter {
   }
 
   public get energy(): Energy {
-    return {
+    return { 
       type_: this._energy.type_,
       amount: this._energy.amount,
     };
+  }
+
+  receiveDamage(attackPoints: number): void | number {
+    const damage = attackPoints - this._defense;
+    if (damage > 0) this._lifePoints -= damage;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+    const isMaxLifePointsValid = this._maxLifePoints > this._race.maxLifePoints;
+    if (isMaxLifePointsValid) {
+      this._maxLifePoints = this._race.maxLifePoints;
+      this._lifePoints = this._maxLifePoints;
+    } else {
+      this._lifePoints = this._race.maxLifePoints;
+    }
+  }
+
+  special(enemy: Fighter): void {
+    if (this._maxLifePoints < 15) {
+      this._maxLifePoints *= 5;
+      enemy.receiveDamage(enemy.lifePoints / 2);
+    } 
   }
 }
